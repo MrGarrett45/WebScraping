7f85e6963803d897af3748555c6bb7b6f48d8dc0
MrGarrett45 <gjmclaughlin19@gwmail.gwu.edu>
2017-06-26 01:06:17 -0400

    Added weather programs

---
+++ dcWeather.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+import webbrowser
+
+webbrowser.open('https://www.google.com/search?client=ubuntu&channel=fs&q=weather+in+boston&ie=utf-8&oe=utf-8#channel=fs&q=weather+in+dc')
